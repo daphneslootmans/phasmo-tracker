8f0c9cae2e39b1932103455e121177ea5e2e45d3
Daphne Slootmans <github@daphneslootmans.be>
2021-09-04 19:35:49 +0200

    Hide navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -3,19 +3,19 @@
     <b-navbar toggleable="lg" type="dark" variant="dark">
       <b-navbar-brand href="#">Phasmophobia Evidence Tracker</b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-form>
-            <b-form-checkbox v-model="lightMode" name="check-light-mode" switch class="mr-3">
-              Light Mode
-            </b-form-checkbox>
-            <b-form-checkbox v-model="ghostInfo" name="check-ghost-info" switch>
-              Ghost Info
-            </b-form-checkbox>
-          </b-nav-form>
-        </b-navbar-nav>
-      </b-collapse>
+<!--      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>-->
+<!--      <b-collapse id="nav-collapse" is-nav>-->
+<!--        <b-navbar-nav>-->
+<!--          <b-nav-form>-->
+<!--            <b-form-checkbox v-model="lightMode" name="check-light-mode" switch class="mr-3">-->
+<!--              Light Mode-->
+<!--            </b-form-checkbox>-->
+<!--            <b-form-checkbox v-model="ghostInfo" name="check-ghost-info" switch>-->
+<!--              Ghost Info-->
+<!--            </b-form-checkbox>-->
+<!--          </b-nav-form>-->
+<!--        </b-navbar-nav>-->
+<!--      </b-collapse>-->
     </b-navbar>
   </div>
 </template>
